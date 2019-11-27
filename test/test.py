4e91869271be7bc8a29ef3a7d039d157dd657db0
Danilo Collares <dundeepessoal@gmail.com>
2019-11-27 19:37:05 -0300

    * detecçao de concorrencia funcional, mudança no padrao de logs

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
-# A ideia desse código é, baseado nos prints, validar as regras de negócio do sistema
+# A "id"eia desse código é, baseado nos prints, val"id"ar as regras de negócio do sistema
 # Baseado nos logs, veremos se há alguma violação de regras, e se tiver poderemos
-# determinar o porquê dela ter acontecido
+# determinar o porquê dela ter acontec"id"o
 
 # Referência de Log
 
@@ -15,6 +15,7 @@ def CheckConditionsForErrors(writerCounter, readerCounter, writerSequence, reade
         print ("Concorrência entre escritores detectada!")
     elif (writerCounter >= 1 and readerCounter >= 1):
         print ("Concorrência entre leitores e escritores detectada!")
+    print("")
 
 def main():
     escCounter = 0
@@ -22,24 +23,34 @@ def main():
     escSequence = 0
     leitSequence = 0
 
+    i = 0
+    pessoasNaVariavel = []
+
     logFileName = input("Nome do arquivo de log: ")
     logFile = open(logFileName,'r')
 
     for line in logFile:
+        print ("Line " + str(i + 1))
         workingLine = line.rsplit()
-
-        if (workingLine[0] is "Esc" and workingLine[2] is "entrou"):
+        if (workingLine[0] in "Esc" and workingLine[2] in "entrou"):
+            pessoasNaVariavel.append({"type":workingLine[0], "id":workingLine[1]})
             escCounter += 1
             escSequence += 1
-        elif (workingLine[0] is "Esc" and workingLine[2] is "saiu"):
+        elif (workingLine[0] in "Esc" and workingLine[2] in "saiu"):
+            pessoasNaVariavel.remove({"type":workingLine[0], "id":workingLine[1]})
             escCounter -= 1
-        if (workingLine[0] is "Leit" and workingLine[2] is "entrou"):
+        if (workingLine[0] in "Leit" and workingLine[2] in "entrou"):
+            pessoasNaVariavel.append({"type":workingLine[0], "id":workingLine[1]})
             leitCounter += 1 
             leitSequence += 1
-        elif (workingLine[0] is "Leit" and workingLine[2] is "saiu"):
+        elif (workingLine[0] in "Leit" and workingLine[2] in "saiu"):
+            pessoasNaVariavel.remove({"type":workingLine[0], "id":workingLine[1]})
             leitCounter -= 1
 
+        for pessoa in pessoasNaVariavel:
+            print(pessoa["type"],pessoa["id"])
 
         CheckConditionsForErrors(escCounter, leitCounter, escSequence, leitSequence)
+        i += 1
 
 main()
