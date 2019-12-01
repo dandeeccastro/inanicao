99d17d762ae5357a6e90c94a093c8efd3afbf13f
Danilo Collares <dundeepessoal@gmail.com>
2019-12-01 19:48:25 -0300

    - semaforos removidos

--- src/min.c
+++ src/min.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <semaphore.h>
 #include <pthread.h>
 #include <string.h>
 
 int num_leituras, num_escritas, quant_leitoras, quant_escritoras;
 int var_global;
-int leitoras_global = 0, escritoras_global = 0;
 FILE * log_file;
-sem_t mutex_leit, mutex_escr, mutex_geral, lendo, escrevendo;
 
 typedef struct {
 	int id;
@@ -35,30 +32,15 @@ typedef struct {
 void * Leitora( void * arg ){
 	int * id = (int *) arg;
 	while (1) {
-		sem_wait(&mutex_leit);
 		if (num_leituras <= 0){
-			sem_post(&mutex_leit);
 			break;
 		} else {
-			sem_post(&mutex_leit);
 
-			sem_wait(&lendo);
-			sem_wait(&mutex_leit);
-			leitoras_global++;
-			if (leitoras_global == 1)
-				sem_wait(&escrevendo);
-			sem_post(&mutex_leit);
-			sem_post(&lendo);
 
 			EntraLeitura(*id);
 			SaiLeitura(*id);
 
-			sem_wait(&mutex_leit);
 			num_leituras--;
-			leitoras_global--;
-			if (leitoras_global == 0)
-				sem_post(&escrevendo);
-			sem_post(&mutex_leit);
 		}
 	}
 	pthread_exit(NULL);
@@ -68,10 +50,8 @@ void EntraLeitura(int id){
 	FILE *leit_file;
 	char *leit_filename;
 
-	sem_wait(&mutex_geral);
 	fprintf(log_file, "Leit %d entrou\n", id);
 	fprintf(log_file, "Leit %d leu %d\n", id, var_global);
-	sem_post(&mutex_geral);
 
 	leit_file = calloc(sizeof(FILE*),1);
 	leit_filename = calloc(sizeof(char),20);
@@ -85,58 +65,37 @@ void EntraLeitura(int id){
 }
 
 void SaiLeitura(int id){
-	sem_wait(&mutex_geral);
 	fprintf(log_file, "Leit %d saiu\n", id);
-	sem_post(&mutex_geral);
 }
 
 void * Escritora ( void * arg ){
 	int * id = (int *) arg;
 	while (1) {
-		sem_wait(&mutex_escr);
 		if (num_escritas <= 0){
-			sem_post(&mutex_escr);
 			break;
 		} else {
-			sem_post(&mutex_escr);
 
-			sem_wait(&mutex_escr);
-			escritoras_global++;
-			if (escritoras_global == 1)
-				sem_wait(&lendo);
-			sem_post(&mutex_escr);
 
-			sem_wait(&escrevendo);
 			EntraEscrita(*id);
 			num_escritas--;
 			SaiEscrita(*id);
-			sem_post(&escrevendo);
 
-			sem_wait(&mutex_escr);
-			escritoras_global--;
-			if (escritoras_global == 0)
-				sem_post(&lendo);
-			sem_post(&mutex_escr);
 		}
 	}
 	pthread_exit(NULL);
 }
 
 void EntraEscrita(int id){
-	sem_wait(&mutex_geral);
 	fprintf(log_file, "Esc %d entrou\n", id);
 	fprintf(log_file, "Esc %d leu %d\n", id, var_global);
 
 	var_global = id;
 
 	fprintf(log_file, "Esc %d escreveu %d\n", id, var_global);	
-	sem_post(&mutex_geral);
 }
 
 void SaiEscrita(int id){
-	sem_wait(&mutex_geral);
 	fprintf(log_file, "Esc %d saiu\n", id);
-	sem_post(&mutex_geral);
 }
 
 int main( int argc, char * argv[]){
@@ -152,11 +111,6 @@ int main( int argc, char * argv[]){
 	num_escritas = atoi(argv[4]);
 	log_file = fopen(argv[5],"w+");
 
-	sem_init(&mutex_escr,0,1);
-	sem_init(&mutex_leit,0,1);
-	sem_init(&mutex_geral,0,1);
-	sem_init(&lendo,0,quant_leitoras);
-	sem_init(&escrevendo,0,1);
 
 	int i, * t;
 	pthread_t threads[quant_leitoras + quant_escritoras];
